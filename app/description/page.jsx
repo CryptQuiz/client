dd19a1d2230d9e4d604c24d4cea56788a85c954d
Samet Gecgel <sgrio1453@gmail.com>
2023-10-16 15:51:34 +0300

    fit created question component

--- app/description/page.jsx
+++ app/description/page.jsx
@@ -18,7 +18,7 @@ const Description = () => {
         {/* sponsor adı ve zaman */}
         <div className='flex-grow mt-8 mx-8'>
           <div className='flex flex-row w-full justify-between'>
-            <div className='w-3/5 px-5 py-auto text-center bg-neu-orange text-black text-32 font-bold rounded-20 border border-black border-r-8 border-b-8'>Truefeedback Quiz Yarışması</div>
+            <div className='w-3/5 px-5 py-1 text-center bg-neu-orange text-black text-32 font-bold rounded-20 border border-black border-r-8 border-b-8'>Truefeedback Quiz Yarışması</div>
             <div className='w-1/4 flex flex-row justify-around items-center bg-neu-green3 font-bold text-black border rounded-20 border-black border-r-8 border-b-8'>
               <div className='text-32'>15 Ekim</div>
               <div className='text-2xl'>
