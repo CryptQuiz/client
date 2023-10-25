0d24f05f037a103f053d68819d5ad3aa2952b949
atesbey-design <tuncerbostancibasi@gmail.com>
2023-10-25 18:06:28 +0300

    Fix:Fetch data framework changed

--- data/Backend.js
+++ data/Backend.js
@@ -1,23 +1,26 @@
 import axios from 'axios';
 import api from './clients';
 
-const BASE_URL = "http://[::1]:32500"
+const BASE_URL = "http://[::1]:32500/"
 
 
-const sendRequest = async (method, url, data) => {
-  const request = await fetch(url, method, data)
-
-  if (request.status === 200) {
-    return request.json()
+const sendRequest = async (method, endpoint, data) => {
+  try {
+    const response = await axios({
+      method,
+      url: `${BASE_URL}${endpoint}`,
+      data,
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
   }
-
-  throw request
-}
+};
 
 const Backend = {
   Root: {
     getRoot: async () => {
-      return await sendRequest('GET', BASE_URL + '/home')
+      return await sendRequest('GET', 'home');
     },
   },
 
