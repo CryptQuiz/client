51ca668037996daf7419cee83b92944a614e2846
atesbey-design <tuncerbostancibasi@gmail.com>
2023-10-25 18:04:51 +0300

    Feat:Generic api entegration

--- data/Backend.js
+++ data/Backend.js
@@ -3,53 +3,24 @@ import api from './clients';
 
 const BASE_URL = "http://[::1]:32500"
 
+
+const sendRequest = async (method, url, data) => {
+  const request = await fetch(url, method, data)
+
+  if (request.status === 200) {
+    return request.json()
+  }
+
+  throw request
+}
+
 const Backend = {
   Root: {
-    getRoot: () => {
-      return axios.get(`${BASE_URL}/home`);
-    }
+    getRoot: async () => {
+      return await sendRequest('GET', BASE_URL + '/home')
+    },
   },
 
-  Projects: {
-    getAllProject: () => {
-      return axios.get(`${BASE_URL}/backoffice/projects/`);
-    },
-    getbyId: (id) => {
-      console.log(id);
-      return axios.get(`${BASE_URL}/backoffice/projects/${id}`);
-    },
-    createProject: (data) => {
-      return axios.post(`${BASE_URL}/backoffice/projects/create`, data);
-    },
-  },
-  Airdrops: {
-    getAllAirdrops: () => {
-      return axios.get(`${BASE_URL}/backoffice/projects/airdrop/`);
-    },
-    getbyId: (id) => {
-      return axios.get(`${BASE_URL}/backoffice/projects/airdrop/${id}`);
-    },
-    createAirdrop: (data) => {
-      return axios.post(`${BASE_URL}/backoffice/projects/airdrop/create`, data);
-    },
-    updateAirdrop: (id, data) => {
-      return axios.post(`${BASE_URL}/backoffice/projects/airdrop/update/${id}`, data);
-    },
-  },
-  Venture: {
-    getAllVenture: () => {
-      return axios.get(`${BASE_URL}/backoffice/venture/`);
-    },
-    getbyId: (id) => {
-      return axios.get(`${BASE_URL}/backoffice/venture/${id}`);
-    },
-    createVenture: (data) => {
-      return axios.post(`${BASE_URL}/backoffice/venture/create`, data);
-    },
-    updateVenture: (id, data) => {
-      return axios.post(`${BASE_URL}/backoffice/venture/update/${id}`, data);
-    },
-  },
 };
 
 export default Backend;
